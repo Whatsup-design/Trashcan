a7203793262b7346500e54fdd0d20966417dff31
Sonson <16001sonson@kajonkietsuksa.ac.th>
2026-04-17 22:33:08 +0700

    'marginal_change'

--- backend/src/controller/auth/login.ts
+++ backend/src/controller/auth/login.ts
@@ -1,25 +1,26 @@
-import { PostLogin } from "../../services/auth/login.js";
-import type { Request, Response } from "express";
+// import { PostLogin } from "../../services/auth/login.js";
+// import type { Request, Response } from "express";
 
-import { generateToken } from "../../utils/generateToken.js";
-export async function loginController(req: Request, res: Response) {
-  try {
+// import { generateToken } from "../../utils/generateToken.js";
+// export async function loginController(req: Request, res: Response) {
+//   try {
 
-    const { username, password } = req.body as {
-      username?: string;
-      password?: string;
-    };
+//     const { username, password } = req.body as {
+//       username?: string;o
 
-    if (!username || !password) {
-      return res.status(400).json({ message: "Username and password are required" });
-    }
+//       password?: string;
+//     };
 
-    const result = await PostLogin(username, password);
+//     if (!username || !password) {
+//       return res.status(400).json({ message: "Username and password are required" });
+//     }
+
+//     const result = await PostLogin(username, password);
 
 
 
-  } catch (error) {
-    console.error("Login error:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+//   } catch (error) {
+//     console.error("Login error:", error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// };
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,16 +1,16 @@
-import express from "express";
+// import express from "express";
 
-import { loginController } from "../controller/auth/login.js";
+// import { loginController } from "../controller/auth/login.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-// router.post("/login", async (req, res) => {
-//   // Implement your login logic here, e.g., validate credentials, generate JWT, etc.
-//   console.log("Received login request with body:", req.body);
+// // router.post("/login", async (req, res) => {
+// //   // Implement your login logic here, e.g., validate credentials, generate JWT, etc.
+// //   console.log("Received login request with body:", req.body);
 
-//   // For demonstration, we'll just return a success response.
-//   res.json({message : "Login successful", payload:req.body});
-// });
-router.post("/login", loginController); 
+// //   // For demonstration, we'll just return a success response.
+// //   res.json({message : "Login successful", payload:req.body});
+// // });
+// router.post("/login", loginController); 
 
-export default router;
+// export default router;
